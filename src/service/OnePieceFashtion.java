531a3bcf6085b1a981cb135c5bf8bfcd25154d93
Trungntph42450 <Trungntph42450@fpt.edu.vn>
2024-03-22 16:35:11 +0700

    kh,dmk

--- src/service/OnePieceFashtion.java
+++ src/service/OnePieceFashtion.java
@@ -1,13 +1,26 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package service;
+
+import java.util.List;
 
 /**
  *
  * @author Nguyen Thanh Trung
  */
-public class OnePieceFashtion {
-    
+public interface OnePieceFashtion<K,E>  {
+
+    public List<K> getAll();
+
+    public int them(K k);
+
+    public int sua(K k, E e);
+
+    public int xoa(E e);
+
+    public K getOne(E e);
+
+    public List<K> getList(E e);
 }
